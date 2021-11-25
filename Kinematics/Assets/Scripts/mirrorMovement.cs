4a30e801ec854c32b7b2b7c09c1e67fa98b23bb4
Ryan <ryan.palazon@enti.cat>
2021-11-25 13:49:16 +0100

    ejercicio 1 swing/twist done

--- Kinematics/Assets/Scripts/mirrorMovement.cs
+++ Kinematics/Assets/Scripts/mirrorMovement.cs
@@ -2,15 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class mirrorMovement : MonoBehaviour
+public class MirrorMovement : MonoBehaviour
 {
     public GameObject original;
-	
-
 
     [Header("Angle Constraint")]
     public bool AngleConstraintActive;
     public bool CancelTwist;
+
     [Range(0.0f, 180.0f)]
     public float maxAngle;
 
@@ -19,7 +18,6 @@ public class mirrorMovement : MonoBehaviour
 
     public Transform parent;
     public Transform child;
-
 
     [Header("Plane Constraint")]
     public bool PlaneConstraintActive;
@@ -30,11 +28,25 @@ public class mirrorMovement : MonoBehaviour
     // To define how "strict" we want to be
     private float threshold = 0.00001f;
 
-
-
-
-    void Update ()
+    void Update()
     {
-        transform.localRotation = original.transform.localRotation;
+        GetSwing(original.transform.localRotation).ToAngleAxis(out float angle, out Vector3 axis);
+
+        angle = Mathf.Clamp(angle, minAngle, maxAngle);
+
+        if (CancelTwist)
+            transform.localRotation = Quaternion.AngleAxis(Mathf.Clamp(angle, minAngle, maxAngle), axis);
+        else
+            transform.localRotation = Quaternion.AngleAxis(Mathf.Clamp(angle, minAngle, maxAngle), axis) * GetTwist(original.transform.localRotation);
+    }
+
+    public Quaternion GetTwist(Quaternion q)
+    {
+        return Quaternion.Normalize(new Quaternion(0, q.y, 0, q.w));
+    }
+
+    public Quaternion GetSwing(Quaternion q)
+    {
+        return q * Quaternion.Inverse(GetTwist(q));
     }
 }
